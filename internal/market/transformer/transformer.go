1b3aac6bcc6e05a96520249d53d9a4f71edb1d32
Carlos Kenny <carloskenny@live.com>
2023-06-21 16:41:39 -0300

    🦄 refactor: ajust class name transform input

--- internal/market/transformer/transformer.go
+++ internal/market/transformer/transformer.go
@@ -5,7 +5,7 @@ import (
 	"github.com/carloskenny/imersao13fullcycle/internal/market/entity"
 )
 
-func TransformInput(input dto.OrderInput) *entity.Order {
+func TransformInput(input dto.TradeInput) *entity.Order {
 	asset := entity.NewAsset(input.AssetID, input.AssetID, 1000)
 	investor := entity.NewInvestor(input.InvestorID)
 	order := entity.NewOrder(input.OrderID, investor, asset, input.Shares, input.Price, input.OrderType)
